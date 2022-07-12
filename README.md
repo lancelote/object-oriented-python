ce0b5646bfc697eca1d01ca9a12940d9c1000533
Pavel Karateev <lancelote.du.lac@gmail.com>
2022-07-12 18:58:13 +0200

    "Higher or Lower Card" game

---
+++ README.md
@@ -0,0 +1,30 @@
+# object-oriented-python
+
+My experiments with examples from "[Object-Oriented Python][book]" book by Irv Kalb
+
+## TOC
+
+- Part I: Introducing Object-Oriented Programming
+  - [ ] [Chapter 1: Procedural Python Examples](src/ch01)
+    - [Higher or Lower Card Game](src/ch01/higher_or_lower_procedural.py)
+  - [ ] Chapter 2: Modeling Physical Objects with Object-Oriented Programming
+  - [ ] Chapter 3: Metal Models of Objects and the Meaning of "self"
+  - [ ] Chapter 4: Managing Multiple Objects
+- Part II: Graphical User Interfaces with Pygame
+  - [ ] Chapter 5: Introduction to Pygame
+  - [ ] Chapter 6: Object-Oriented Pygame
+  - [ ] Chapter 7: Pygame GUI Widgets
+- Part III: Encapsulation, Polymorphism, and Inheritance
+  - [ ] Chapter 8: Encapsulation
+  - [ ] Chapter 9: Polymorphism
+  - [ ] Chapter 10: Inheritance
+  - [ ] Chapter 11: Managing Memory Used by Objects
+- Part IV: Using OOP in Game Development
+  - [ ] Chapter 12: Card Games
+  - [ ] Chapter 13: Timers
+  - [ ] Chapter 14: Animation
+  - [ ] Chapter 15: Scenes
+  - [ ] Chapter 16: Full Game: Dodger
+  - [ ] Chapter 17: Design Patterns and Wrap-Up
+
+[book]: https://www.goodreads.com/book/show/59606992-object-oriented-python
---
+++ src/ch01/higher_or_lower_procedural.py
@@ -0,0 +1,126 @@
+import random
+from typing import TypedDict
+from typing import TypeAlias
+
+Rank: TypeAlias = str
+Suit: TypeAlias = str
+Deck: TypeAlias = list["Card"]
+
+
+class Card(TypedDict):
+    rank: Rank
+    suit: Suit
+    value: int
+
+
+SUITS = ("Spades", "Hearts", "Clubs", "Diamonds")
+RANKS = (
+    "Ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King",
+)
+N_CARDS = 8
+
+
+def get_card(deck_in: Deck) -> Card:
+    this_card = deck_in.pop()
+    return this_card
+
+
+def shuffle(deck_list_in: Deck) -> Deck:
+    deck_out = deck_list_in.copy()
+    random.shuffle(deck_out)
+    return deck_out
+
+
+def create_deck() -> Deck:
+    deck = []
+    for suit in SUITS:
+        for this_value, rank in enumerate(RANKS):
+            card = {"rank": rank, "suit": suit, "value": this_value + 1}
+            deck.append(card)
+    return deck
+
+
+def main() -> None:
+    print("Welcome to Higher or Lower")
+    print(
+        "You have to choose whether the next card to be shown "
+        "will be higher or lower than the current card"
+    )
+    print(
+        "Getting it right adds 20 points; get it wrong and "
+        "you lose 15 points."
+    )
+    print("You have 50 points to start.")
+    print()
+
+    starting_deck = create_deck()
+    score = 50
+
+    while True:
+        print()
+        game_deck = shuffle(starting_deck)
+
+        current_card = get_card(game_deck)
+        current_rank = current_card["rank"]
+        current_suit = current_card["suit"]
+        current_value = current_card["value"]
+
+        print(f"Starting card is: {current_rank} of {current_suit}")
+        print()
+
+        for card_number in range(N_CARDS):
+            answer = input(
+                f"Will the next card be higher or lower than the "
+                f"{current_rank} of {current_suit}? "
+                f"(enter h or l): "
+            )
+            answer = answer.casefold()
+            next_card = get_card(game_deck)
+            next_rank = next_card["rank"]
+            next_suit = next_card["suit"]
+            next_value = next_card["value"]
+
+            print(f"Next card is: {next_rank} of {next_suit}")
+
+            if answer == "h":
+                if next_value > current_value:
+                    print("You got it right, it was higher")
+                    score += 20
+                else:
+                    print("Sorry, it was not higher")
+                    score -= 15
+            elif answer == "l":
+                if next_value < current_value:
+                    print("You got it right, it was higher")
+                    score += 20
+                else:
+                    print("Sorry, it was not higher")
+                    score -= 15
+
+            print(f"Your score is {score}")
+            print()
+
+            current_rank = next_rank
+            current_value = next_value
+
+        again = input("To play again, press ENTER, or q to quit: ")
+        if again == "q":
+            break
+
+    print("OK bye")
+
+
+if __name__ == "__main__":
+    main()
